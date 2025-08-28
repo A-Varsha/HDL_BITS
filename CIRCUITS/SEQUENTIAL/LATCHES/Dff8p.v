d9685a41760b3881b1a6b6b118cb4a8a7b3e9adb
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-08-28 22:05:32 +0530

    Create Dff8p.v

---
+++ CIRCUITS/SEQUENTIAL/LATCHES/Dff8p.v
@@ -0,0 +1,19 @@
+module top_module (
+    input clk,
+    input reset,
+    input [7:0] d,
+    output [7:0] q
+);
+integer i;   // loop variable
+     always @(negedge clk) begin
+        if (reset) begin
+            q <= 8'h34; // Reset value: 0x34
+        end else begin
+            q <= d;
+        end
+    end
+
+endmodule
+
+
+
