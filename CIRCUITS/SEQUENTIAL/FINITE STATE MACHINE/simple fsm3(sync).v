730f2a3caaea810dce3b2344c112129577168564
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-09-04 18:25:13 +0530

    Create simple fsm3(sync).v

---
+++ CIRCUITS/SEQUENTIAL/FINITE STATE MACHINE/simple fsm3(sync).v
@@ -0,0 +1,28 @@
+module top_module(
+    input clk,
+    input in,
+    input reset,
+    output out); //
+
+    reg [2:0]state,next_state;
+    parameter A=1,B=2,C=3,D=4;
+     always@(*)begin
+        case(state)
+            A:next_state=(in==1)?B:A;
+            B:next_state=(in==1)?B:C;
+            C:next_state=(in==1)?D:A;
+            D:next_state=(in==1)?B:C;
+        endcase
+    end
+    always @ (posedge clk)begin
+        if(reset)begin
+            state<=A;
+            end else
+                state<=next_state;
+    end
+   
+    assign out=(state==D);
+endmodule
+
+
+
