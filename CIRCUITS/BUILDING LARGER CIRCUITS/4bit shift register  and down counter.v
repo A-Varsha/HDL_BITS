6c172e00eb4b865179773353c40c5cd56350a573
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-09-12 16:17:07 +0530

    Create 4bit shift register  and down counter.v

---
+++ CIRCUITS/BUILDING LARGER CIRCUITS/4bit shift register  and down counter.v
@@ -0,0 +1,16 @@
+module top_module (
+    input clk,
+    input shift_ena,
+    input count_ena,
+    input data,
+    output [3:0] q);
+    reg [3:0] shift_reg;
+    always @ (posedge clk)begin
+        if (shift_ena)
+            shift_reg<={shift_reg[2:0],data};
+       else if(count_ena)begin
+            shift_reg<=shift_reg-1;
+    end
+    end
+    assign q=shift_reg;
+endmodule
